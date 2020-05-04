c1970bd1c234554318d77bd7221349dff353f38a
feridun Akyol <feridunakyol34@gmail.com>
2020-05-04 16:38:14 +0200

    solution

--- isolate/fs-exercises/exercise-8.js
+++ isolate/fs-exercises/exercise-8.js
@@ -22,62 +22,61 @@ FLAGS:
 `;
 
 // declare logging function
-const log = (logId, value) => console.log(
-  `\nlog ${logId}, ${Date.now() - START} ms. \n`,
-  value
-);
-
+const log = (logId, value) => console.log(`\nlog ${logId}, ${Date.now() - START} ms. \n`, value);
 
 // --- main script ---
 
 // fill in the _'s to reverse-engineer the behavior of exercise-8-demo.min.js
 
 if (process.argv.includes('-h')) {
-  log(0, DOC_STRING);
-  process.exit(0);
-};
+	log(0, DOC_STRING);
+	process.exit(0);
+}
 
-const command = _;
-const fileName = _;
+const command = process.argv[2];
+const fileName = process.argv[3];
 
-if (_) {
-  log('1.a', 'a command is required, exiting');
-  process.exit(0);
+if (command == null) {
+	log('1.a', 'a command is required, exiting');
+	process.exit(0);
 }
 log('1.b', 'command: ' + command);
 
+if (command === 'list') {
+	log('3', 'reading filenames ...');
+	const fileNames = fs.readdirSync(__dirname);
+	log('4', fileNames);
+	process.exit(0);
+}
 
-if (command === _) {
-  log('3', 'reading filenames ...');
-  const fileNames = fs.readdirSync(_);
-  log('4', fileNames);
-  process.exit(0)
-};
-
-if (_) {
-  log('2.a', 'a file name is required, exiting');
-  process.exit(0);
+if ((command === 'read' || command === 'unlink') && process.argv[3] == null) {
+	log('2.a', 'a file name is required, exiting');
+	process.exit(0);
 }
 log('2.b', 'fileName: ' + fileName);
 
-
-
-if (_) {
-  log('3.a', 'declaring _');
-  const _ = (err, contents) => {
-    _;
-  };
-  fs._(__dirname + '/' + fileName, 'utf-8', _);
-  log('4.a', '_ from ' + fileName + ' ...');
-
-} else if (_) {
-  log('3.b', 'declaring _');
-  const _ = (err) => {
-    _;
-  };
-  fs._(__dirname + '/' + fileName, _);
-  log('4.b', '_ ' + fileName + ' ...');
-
+if (command === 'read') {
+	log('3.a', 'declaring reading file');
+	const readCallback = (err, contents) => {
+		if (err) {
+			log(3, err);
+			return;
+		} else {
+			log(contents);
+		}
+	};
+	fs.readFile(__dirname + '/' + fileName, 'utf-8', readCallback);
+	log('4.a', 'read contents from ' + fileName + ' ...');
+} else if (command === 'unlink') {
+	log('3.b', 'declaring unlink');
+	const deleteCallBack = (err) => {
+		if (err) {
+			log(err);
+			return;
+		}
+	};
+	fs.unlink(__dirname + '/' + fileName, deleteCallBack);
+	log('4.b', 'delete' + fileName + ' ...');
 } else {
-  log('3.c', 'unknown command: ' + command);
+	log('3.c', 'unknown command: ' + command);
 }
