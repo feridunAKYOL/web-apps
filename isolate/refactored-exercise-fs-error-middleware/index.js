01b71ef7731ea14205012755e853cd76c2dab873
feridun Akyol <feridunakyol34@gmail.com>
2020-05-19 15:06:29 +0200

    refactored-exercise-fs-error-middleware

--- isolate/refactored-exercise-fs-error-middleware/index.js
+++ isolate/refactored-exercise-fs-error-middleware/index.js
@@ -5,10 +5,11 @@ const path = require('path')
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const config = require('./config');
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/error-handler');
-
+const api = require('./api/routes')
 
 const FILE_DIR = path.join(__dirname, config.FILES_DIR);
 
@@ -17,42 +18,14 @@ const app = express();
 app.use(logger);
 app.use(bodyParser.json());
 
+
 app.get('/', (req, res) => {
   res.send('home page!');
 });
 
-app.get('/api/list', (req, res, next) => {
+app.use('/api' , api);
 
-  fs.readdir(FILE_DIR, (err, data) => {
 
-    if (err) {
-      next(err)
-      return
-    }
-
-    console.log('this is the directory listing', data)
-    res.json(data)
-  })
-})
-
-app.post('/api/create', (req, res, next) => {
-
-  const name = req.query.name
-
-  // check if file already exists
-
-  const content = req.body.fileContent
-
-  fs.writeFile(`${FILE_DIR}/${name}`, content, (err) => {
-
-    if (err) {
-      next(err)
-      return
-    }
-
-    res.sendStatus(200);
-  })
-})
 
 app.use(errorHandler)
 
