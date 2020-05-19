48140265435254f7d1d99ca7580a89d13c2bf5c5
feridun Akyol <feridunakyol34@gmail.com>
2020-05-19 15:51:38 +0200

    refactored-exercise-fs-param-query-body

--- isolate/refactored-exercise-fs-param-query-body/api/routes.js
+++ isolate/refactored-exercise-fs-param-query-body/api/routes.js
@@ -2,7 +2,7 @@ const handlers = require('./handlers.js');
 const express = require('express');
 
 // create the router
-_;
+const router = express.Router() ;
 
 router.use((req, res, next) => {
   console.log('routes!');
@@ -15,6 +15,12 @@ router.get('/', (req, res) => {
 
 // write the routes!
 
+
+router.post('/param/:value', handlers.postParam);
+
+router.post('/query', handlers.postQuery);
+
+router.post('/body', handlers.postBody);
 
 
 module.exports = router;
--- isolate/refactored-exercise-fs-param-query-body/index.js
+++ isolate/refactored-exercise-fs-param-query-body/index.js
@@ -13,6 +13,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const config = require('./config');
+const api = require('./api/routes');
 
 const FILES_DIR = path.join(__dirname, config.FILES_DIR);
 
@@ -34,75 +35,8 @@ app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'client')));
 
 // refactor these routes into /api
-app.get('/api', (req, res) => {
-  res.send('api!');
-});
 
-app.post('/api/param/:value', (req, res) => {
-  const paramValue = req.params.value;
-
-  console.log(`param value: ${paramValue}`);
-
-  const fileName = 'param.txt';
-  fs.writeFile(`${FILES_DIR}/${fileName}`, paramValue, err => {
-    if (err && err.code === 'ENOENT') {
-      console.log(err);
-      res.status(404).end();
-      return;
-    }
-    if (err) {
-      console.log(err);
-      next(err);
-      return;
-    }
-
-    res.json({ message: `'${paramValue}' saved to ${fileName}` });
-  });
-});
-
-app.post('/api/query', (req, res) => {
-  const queryValue = req.query.value;
-
-  console.log(`query value: ${queryValue}`);
-
-  const fileName = 'query.txt';
-  fs.writeFile(`${FILES_DIR}/${fileName}`, queryValue, err => {
-    if (err && err.code === 'ENOENT') {
-      console.log(err);
-      res.status(404).end();
-      return;
-    }
-    if (err) {
-      console.log(err);
-      next(err);
-      return;
-    }
-
-    res.json({ message: `'${queryValue}' saved to ${fileName}` });
-  });
-});
-
-app.post('/api/body', (req, res) => {
-  const bodyValue = req.body.value;
-
-  console.log(`body value: ${bodyValue}`);
-
-  const fileName = 'body.txt';
-  fs.writeFile(`${FILES_DIR}/${fileName}`, bodyValue, err => {
-    if (err && err.code === 'ENOENT') {
-      console.log(err);
-      res.status(404).end();
-      return;
-    }
-    if (err) {
-      console.log(err);
-      next(err);
-      return;
-    }
-
-    res.json({ message: `'${bodyValue}' saved to ${fileName}` });
-  });
-});
+app.use('/api' , api);
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
